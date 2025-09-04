a80b1bd3cbe2e58c8bcb6db6bd803a4cedfaaf61
Cauã Melgarejo <cauamelgarejo01@gmail.com>
2025-09-04 01:23:45 -0300

    ajustes deploy 2

--- movies-app/src/app/movie/[id]/page.tsx
+++ movies-app/src/app/movie/[id]/page.tsx
@@ -1,7 +1,12 @@
 import MovieDetail from "@/components/MovieDetails";
+import { Metadata } from "next";
+
+interface Params {
+  id: string;
+}
 
 interface PageProps {
-  params: { id: string };
+  params: Params;
 }
 
 export default async function Details({ params }: PageProps) {
--- movies-app/src/types/movie.ts
+++ movies-app/src/types/movie.ts
@@ -5,3 +5,27 @@ export interface Movie {
   overview:string,
   vote_average:number,
 }
+export interface Cast {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+}
+
+export interface Crew {
+  job: string;
+  name: string;
+}
+
+export interface MovieDetails {
+  id: number;
+  title: string;
+  poster_path: string;
+  backdrop_path: string;
+  overview: string;
+  vote_average: number;
+  credits?: {
+    cast: Cast[];
+    crew: Crew[];
+  };
+}
